82042624ec6d94ca84ca2ac96bd4efc856d2559d
KimSM <fakercat@naver.com>
2022-01-13 16:06:01 +0900

    Add paging event

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 
+// axios.defaults.baseURL = 'http://localhost:8080'; //서버주소
+// axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 Vue.prototype.$http = axios
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
